8a5ffda2dad7c154924981649f5f764d2dc7bb82
Do-yoon <do.yoon.like.cheeze@gmail.com>
2021-09-13 23:30:04 +0900

    home update

--- src/component/ChatRoomTable.tsx
+++ src/component/ChatRoomTable.tsx
@@ -1,48 +1,50 @@
 import 'css/MainLayout.css'
-import * as _ from 'lodash';
-import Constant from 'util/Constant'
 import {useEffect, useState} from 'react';
 
-const columns = [
+interface RoomBannerProps {
+    title: string
+    limit: number
+    current_people: number
+}
+
+const columns: RoomBannerProps[] = [
     {
         title: 'roomName',
-        dataIndex: 'name',
-        key: 'name',
-        width: 100
+        limit: 1,
+        current_people: 100
     },
     {
-        title: 'roomName2',
-        dataIndex: 'name2',
-        key: 'name2',
-        width: 100
-    }
-    , {
         title: 'roomName',
-        dataIndex: 'name',
-        key: 'name',
-        width: 100
-    },
-    {
-        title: 'roomName2',
-        dataIndex: 'name2',
-        key: 'name2',
-        width: 100
-    }, {
+        limit: 2,
+        current_people: 100
+    },{
         title: 'roomName',
-        dataIndex: 'name',
-        key: 'name',
-        width: 100
-    },
-    {
-        title: 'roomName2',
-        dataIndex: 'name2',
-        key: 'name2',
-        width: 100
-    }, {
+        limit: 50,
+        current_people: 100
+    },{
         title: 'roomName',
-        dataIndex: 'name',
-        key: 'name',
-        width: 100
+        limit: 34,
+        current_people: 100
+    },{
+        title: 'roomName',
+        limit: 12,
+        current_people: 100
+    },{
+        title: 'roomName',
+        limit: 154,
+        current_people: 100
+    },{
+        title: 'roomName',
+        limit: 1453,
+        current_people: 100
+    },{
+        title: 'roomName',
+        limit: 341,
+        current_people: 100
+    },{
+        title: 'roomName',
+        limit: 4,
+        current_people: 100
     },
 ]
 let data = [
@@ -69,14 +71,51 @@ let data = [
     }
 ]
 
+function RoomBanner(
+    data: RoomBannerProps
+): JSX.Element{
+    return (
+        <div className="room-banner">
+            <span className="title">{data.title}</span>
+            <span className="limit">{data.limit}</span>
+            <span className="current-people">{data.current_people}</span>
+        </div>
+    );
+}
+/*
+function randomRender(
+    roomData: RoomBannerProps[],
+    n: number,
+
+): JSX.Element {
+    const sparseMatrix = []
+}
+*/
 function getTable(row: number, col: number) {
-    const table_col = [...Array(col)].map((e, i) => <td className={`chatRoomCol${i}`}></td>)
-    const table_row = [...Array(row)].map((e, i) => <tr className={`chatRoomRow${i}`}>{table_col}</tr>)
+    const room_cnt = columns.length;
+    let row_cnt = Math.floor(room_cnt / col);
+    let table = [];
+    for(let i=0; i<row; i++) {
+        if (row_cnt <= 0)
+            break;
+        const t = [...columns.slice(i * col, (i+1) * col)].map((e, idx) => <td className={`chatRoomCol${idx}`}></td>)
+        table.push(<tr className={`chatRoomRow${i}`}>{t}</tr>)
+        row_cnt --;
+    }
+
+    let table_row = null;
+    if (row_cnt < row) {
+        const table_col = [...Array(col)].map((e, i) => <td className={`chatRoomCol${i}`}></td>)
+        table_row = [...Array(row - row_cnt)].map((e, i) => <tr className={`chatRoomRow${i}`}>{table_col}</tr>)
+        table.push(table_row)
+    }
+
 
     return (
         <table className="chat-room-table">
             <thead/>
             <tbody>
+            {table}
             {table_row}
             </tbody>
         </table>
