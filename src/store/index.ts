b532c4b51efd6cd1f6cae960ca8c6500b32b65b5
Do-yoon <do.yoon.like.cheeze@gmail.com>
2022-08-26 16:54:29 +0900

    redux-toolkit 적용

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import UserReducer from "store/user/reducer";
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import ChatReducer from "store/chat/reducer";
-import PageReducer from "store/page/reducer";
+import PageReducer from "store/reducer";
 
 const rootReducer = combineReducers({
         users: UserReducer,
